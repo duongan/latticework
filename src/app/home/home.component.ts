a7badea3c7a5b7f889e0502addc4344ca9fdef60
An Duong <an.duong@terralogic.com>
2019-11-14 09:19:02 +0700

    missing code while merging

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-home',
