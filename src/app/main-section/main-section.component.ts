67fe2624e0a39442f5c46c8b2e538fbb7d5d74a6
Anh Ha <anh.ha@terralogic.com>
2019-11-29 15:45:11 +0700

    Update code

--- src/app/main-section/main-section.component.ts
+++ src/app/main-section/main-section.component.ts
@@ -3,6 +3,7 @@ import { AmberService } from '../services/amber.service';
 import { Amber } from '../types/amber';
 import { AppService } from '../services/app.service';
 import { App } from '../types/app';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-main-section',
@@ -15,28 +16,34 @@ export class MainSectionComponent implements OnInit {
   thirdPanelOpenState = false;
   public amberInfo: Amber;
   public appInfo: App;
-  public showDetailPanel = false;
 
   constructor(
     private amberService: AmberService,
-    private appService: AppService
+    private appService: AppService,
+    private userService: UserService
   ) {}
 
   ngOnInit() {
+    this.userService.currentUser.subscribe(() => {
+      this.amberInfo = null;
+      this.appInfo = null;
+    });
     this.amberService.amberInfo.subscribe((result: Amber) => {
       if (!result) {
+        this.amberInfo = null;
+        this.appInfo = null;
         return;
       }
-      this.showDetailPanel = true;
       this.amberInfo = result;
       this.appInfo = null;
     });
 
     this.appService.appInfo.subscribe((result: App) => {
       if (!result) {
+        this.amberInfo = null;
+        this.appInfo = null;
         return;
       }
-      this.showDetailPanel = true;
       this.appInfo = result;
       this.amberInfo = null;
     });
